6b633cb7f2d478717baa00f045e23f86c2696f7f
Zachary Farnes <zachary.farnes@gmail.com>
2020-11-19 15:43:55 +0000

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Zachary Farnes on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
