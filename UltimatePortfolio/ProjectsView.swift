de536b251f5b0b13aabaeba42d3759bb9288fbc8
Zachary Farnes <zacharyfarnes@Zacharys-MacBook-Pro.local>
2022-05-09 21:01:36 +0100

    Add ability to store tab selection

--- UltimatePortfolio/ProjectsView.swift
+++ UltimatePortfolio/ProjectsView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    static let openTag: String? = "Open"
+    static let closedTag: String? = "Closed"
     let showClosedProjects: Bool
     let projects: FetchRequest<Project>
     
