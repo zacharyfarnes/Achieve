7ac090f6e41713843638f0ea578813633126d683
Zachary Farnes <zachary.farnes@gmail.com>
2022-06-05 21:17:43 +0100

    Add awards tab

---
+++ UltimatePortfolio/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  UltimatePortfolio
+//
+//  Created by Zachary Farnes on 05/06/2022.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
