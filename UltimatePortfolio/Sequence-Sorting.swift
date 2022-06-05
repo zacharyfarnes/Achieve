056c742ec58e2221824258d18cfc6b722356ff69
Zachary Farnes <zachary.farnes@gmail.com>
2022-06-05 14:44:23 +0100

    Add custom sorting for items

---
+++ UltimatePortfolio/Sequence-Sorting.swift
@@ -0,0 +1,20 @@
+//
+//  Sequence-Sorting.swift
+//  UltimatePortfolio
+//
+//  Created by Zachary Farnes on 03/06/2022.
+//
+
+import Foundation
+
+extension Sequence {
+    func sorted<Value>(by keyPath: KeyPath<Element, Value>, using areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows -> [Element] {
+        try self.sorted {
+            try areInIncreasingOrder($0[keyPath: keyPath], $1[keyPath: keyPath])
+        }
+    }
+    
+    func sorted<Value: Comparable>(by keyPath: KeyPath<Element, Value>) -> [Element] {
+        self.sorted(by: keyPath, using: <)
+    }
+}
