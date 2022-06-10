eeeca14a8f56df1afc86bb96e42fb633a411ceea
Zachary Farnes <zachary.farnes@gmail.com>
2022-06-10 14:32:38 +0100

    Add improvements to the Home tab

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Zachary Farnes on 10/06/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
