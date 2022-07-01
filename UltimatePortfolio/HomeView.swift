bb00297aa6e44129f02f2731bd9ec192326ec096
Zachary Farnes <zachary.farnes@gmail.com>
2022-07-01 14:10:41 +0100

    Add code documentation.

--- UltimatePortfolio/HomeView.swift
+++ UltimatePortfolio/HomeView.swift
@@ -24,6 +24,7 @@ struct HomeView: View {
     }
 
     init() {
+        // Construct a fetch request to show the 10 highest-priority, incomplete items from open projects.
         let request: NSFetchRequest<Item> = Item.fetchRequest()
 
         let completedPredicate = NSPredicate(format: "completed = false")
