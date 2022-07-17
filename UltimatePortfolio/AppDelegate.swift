585585a478a1d2f74c611f88b5841bf989e2cd07
Zachary Farnes <zachary.farnes@gmail.com>
2022-07-17 16:42:13 +0100

    Add a Home Screen quick action for adding a new project.

---
+++ UltimatePortfolio/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  UltimatePortfolio
+//
+//  Created by Zachary Farnes on 16/07/2022.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
+}
